9adb76781fd9d23d04c564daf12ca4a6091bcbe7
Paul Wood <pmwood@pm.me>
2022-08-13 20:13:58 -0700

    Added get_classes function

--- libclang_test/app/main.py
+++ libclang_test/app/main.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 from typing import List, Optional
-from clang.cindex import Index
+
+from clang.cindex import Cursor, CursorKind, Index
 
 def dir_contains(dir: Path, patterns: List[str | Path]) -> bool:
     for pat in patterns:
@@ -21,9 +22,18 @@ def find_project_root(patterns: Optional[List[str | Path]]=None) -> Optional[Pat
     return cwd
 
 def traverse(node, depth=0):
-    print(f'{"  " * depth} displayname={node.displayname}, kind={node.kind}, type={node.type.spelling}, size={node.type.get_size()}')
+    print(f'{"  " * depth} displayname={node.displayname}, kind={node.kind}, type={node.type.spelling}')
     for child in node.get_children():
         traverse(child, depth + 1)
+
+def get_classes(cursor: Cursor) -> List[Cursor]:
+    classes = []
+
+    for child in cursor.get_children():
+        if child.kind == CursorKind.STRUCT_DECL or child.kind == CursorKind.CLASS_DECL:
+            classes.append(child)
+
+    return classes
 
 def main():
     prj_root = find_project_root()
@@ -31,5 +41,13 @@ def main():
 
     index = Index.create()
     tu = index.parse('src/test.cpp')
+    hu = index.parse('src/test.h')
     cursor = tu.cursor
-    traverse(cursor)
+    classes = get_classes(cursor)
+    print('Found classes or structs')
+    print(list(tu.get_includes()))
+    for c in classes:
+        print(f'{c.displayname} {c.type.get_size()} bytes')
+        for child in c.get_children():
+            print(f'   name={child.displayname}, type={child.type.spelling}, size={child.type.get_size()}, kind={child.kind}')
+
